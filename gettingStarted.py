f0cc54557c23be7aa1acb25c8a8a9347ad80e464
adimehtanyu <am14420@nyu.edu>
2024-06-01 17:30:36 -0400

    updated answer

--- gettingStarted.py
+++ gettingStarted.py
@@ -12,7 +12,7 @@ def welcome_assignment_answers(question, answer=None):
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
@@ -22,7 +22,7 @@ def welcome_assignment_answers(question, answer=None):
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
         answer = int(1)
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = int(4)
+        answer = int(3)
     else:
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
